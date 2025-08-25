ee41d8343076d0362898460d9268dd70543c7e07
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-08-25 21:39:49 +0530

    updated code

--- Array/FindUnique.java
+++ Array/FindUnique.java
@@ -22,7 +22,7 @@ public class FindUnique {
 
 
     public static void main(String[] args) {
-        Scanner sc new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n = sc.nextInt();
         int arr[]= new int[n];
