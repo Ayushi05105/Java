cf38fdb73d41bbed1aabaa744a12be54ac2daa77
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-09-23 18:50:41 +0530

    updated code

--- Array/sumofArray.java
+++ Array/sumofArray.java
@@ -1,24 +1,15 @@
-
-
-class Array {
-    void sumofArray(){
-        int []arr = {1,5,4,6,7,9};
-        int sum = 0;
-
-        for(int i= 0; i<arr.length ; i++){
+public class sumofArray {
+    static void sumofarray(int[] arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
             sum = sum + arr[i];
         }
         System.out.println(sum);
-
     }
-
-    
-}
-
-public class sumofArray {
     public static void main(String[] args) {
-        Array obj = new Array();
-        obj.sumofArray();
+        int[] arr={1,5,6,4,7,9};
+        sumofarray(arr);
     }
-    
 }
+
+  
