10e6deca5ffdb55c908e391b015503c0089733ae
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-11-22 18:53:38 +0530

    updated code

--- oops/fraction.java
+++ oops/fraction.java
@@ -2,38 +2,53 @@ package oops;
 
 public class fraction {
 
-    
-
-
-
-    int num;
-    int den;
-    public fraction(int num,int den){
-        this.num=num;
-        this.den=den;
+    public static Fraction add(Fraction f1, Fraction f2) {
+        int numerator = f1.num * f2.den + f1.den + f2.num;
+        int denominator = f1.den * f2.den;
+        Fraction f3 = new Fraction(numerator, denominator);
+        return f3;
     }
+
+    public static Fraction multiply(Fraction f1, Fraction f2) {
+        int numerator = f1.num * f2.num;
+        int denominator = f1.den * f2.den;
+        Fraction f3 = new Fraction(numerator, denominator);
+        return f3;
+    }
+
+    public static int gcd(int num, int den) {
+        int min = Math.min(num, den);
+        for (int i = min; i >= 1; i--) {
+            if (num % i == 0 && den % i == 0)
+                return i;
+        }
+        return min;
+    }
+
+    public static class Fraction {
+        int num;
+        int den;
+
+        public Fraction(int num, int den) {
+            this.num = num;
+            this.den = den;
+            simplify();
+        }
+
+        public void simplify() {
+            int hcf = gcd(num, den);
+            num /= hcf;
+            den /= hcf;
+        }
+    }
+
     public static void main(String[] args) {
-        fraction f1 = new fraction(7, 3);
-        System.out.println(f1.num+"/"+f1.den);
-
-    }
-    
-    
-    
-    public class Fraction {
-    int num;
-    int den;
-    public Fraction(int num,int den){
-        this.num=num;
-        this.den=den;
+        Fraction f1 = new Fraction(5, 7);
+        System.out.println(f1.num + "/" + f1.den);
+        Fraction f2 = new Fraction(7, 5);
+        System.out.println(f2.num + "/" + f2.den);
+        Fraction f3 = multiply(f1, f2);
+        System.out.println(f3.num + "/" + f3.den);
     }
 
-    public static void main(String[] args) {
-        Fraction f1=new Fraction(3, 7);
-        System.out.println(f1.num+"/"+f1.den);
-    }
-    
-}
-
-    
 }
