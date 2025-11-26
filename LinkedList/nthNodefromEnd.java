78316ee81b6d8b8a9dabb1ebc1ae662e4befb445
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-11-26 23:26:58 +0530

    updated code

---
+++ LinkedList/nthNodefromEnd.java
@@ -0,0 +1,60 @@
+package LinkedList;
+
+public class nthNodefromEnd {
+
+    public static Node nthNode(Node head , int n){
+        int size =0;
+        Node temp =head;
+        while(temp!=null){
+            size++;
+            temp = temp.next;
+        }
+        int m = size -n +1;
+        temp = head;
+        for(int i =1;i<=m-1;i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    // in 1 traversal turtle problem
+    public static Node nthNode2(Node head,int n){
+        Node slow = head;
+        Node fast = head;
+        for(int i =1;i<=n;i++){
+            fast = fast.next;
+        }
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(100);
+        Node b = new Node(50);
+        Node c = new Node(13);
+        Node d = new Node(34);
+        Node e = new Node(89);
+        Node f = new Node(2);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        Node r = nthNode(a, 2);
+        System.out.println(r.data);
+        Node m = nthNode2(a, 2);
+        System.out.println(m.data);
+
+    }
+    
+}
