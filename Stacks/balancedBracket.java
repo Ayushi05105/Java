1efbe1f4c7ea2188b9e51d642021127870909a19
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-17 22:10:03 +0530

    updated code

--- Stacks/balancedBracket.java
+++ Stacks/balancedBracket.java
@@ -1,6 +1,5 @@
 package Stacks;
 import java.util.*;
-import java.util.Stack;
 public class balancedBracket {
 
     public static boolean isBalanced(String str){
@@ -19,10 +18,28 @@ public class balancedBracket {
         if(st.size()>0) return false;
         else return true;
     }
+//{{}}[{]}
+        public static boolean isBalancedbracket(String str){
+        Stack<Character> st = new Stack<>();
+        int n = str.length();
+        for(int i=0;i<n;i++){
+            char ch = str.charAt(i);
+            if(ch =='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.size()==0) return false;
+                if(st.peek()=='('||st.peek()=='{' ||st.peek() =='[')  st.pop();
+            }
+        }
+        if(st.size()>0) return false;
+        else return true;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        System.out.println(isBalanced(str));
+        //System.out.println(isBalanced(str));
+        System.out.println(isBalancedbracket(str));
         sc.close();
 
     }
