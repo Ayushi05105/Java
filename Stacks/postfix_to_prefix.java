709ee6e078a975a6ceb8fc3870627c4ebc2be73a
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-19 18:37:50 +0530

    updated code

---
+++ Stacks/postfix_to_prefix.java
@@ -0,0 +1,28 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class postfix_to_prefix {
+
+      public static void main(String[] args) {
+        String str = "953+4*6/-";
+        Stack<String> val = new Stack<>();
+        int n = str.length();
+        for(int i=0;i<n;i++){
+            char ch = str.charAt(i);
+            int ascii = (int)ch;
+        if(ascii>=48 && ascii<=57){
+            val.push(ch+"");
+            }else{
+                String v2 = val.pop();
+                String v1 = val.pop();
+                char op = ch;
+                String t = op + v1 + v2;
+                val.push(t);
+            }
+        }   
+        System.out.println(val.peek());   
+      }
+    
+    
+}
