d6fa3a76df13bb524976cea09cbc56909585ce45
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-21 01:33:42 +0530

    updated code

--- Array/arrayList/insert.java
+++ Array/arrayList/insert.java
@@ -12,7 +12,20 @@ public class insert {
         l1.add(7);
         l1.add(8);
 
+        //get an element at index i
+        System.out.println(l1.get(1));
+
+        //print with for loop
+        for(int i=0;i<l1.size();i++){
+            System.out.print(l1.get(i)+" ");
+        }
+        System.out.println();
+
         //printing the array list directly
+        System.out.println(l1);
+
+        // adding element at some index
+        l1.add(1,100);
         System.out.println(l1);
 
         //modifying element at index i
@@ -22,6 +35,22 @@ public class insert {
         //removing an element at index i
         l1.remove(1);
         System.out.println(l1);
+
+        //removing an element e
+        l1.remove(Integer.valueOf(7));
+        System.out.println(l1);
+
+        //checking if an element exists
+        boolean ans = l1.contains(Integer.valueOf(6));
+        System.out.println(ans);
+
+        //if you don't specify class, you can put anything inside l
+        ArrayList l = new ArrayList();
+        l.add("abcd");
+        l.add(1);
+        l.add(true);
+        System.out.println(l);
+
 
 
     }
