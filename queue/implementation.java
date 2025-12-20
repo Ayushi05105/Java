6b53c7027ebf01e0e8e32f3baa51a12b4bb54539
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-20 18:45:41 +0530

    updated code

---
+++ queue/implementation.java
@@ -0,0 +1,26 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class implementation {
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> r = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        while(q.size()>0){
+            System.out.print(q.peek()+" ");
+            r.add(q.poll());
+
+        }
+        while(r.size()>0){
+            q.add(r.poll());
+        }
+    }
+    
+}
