254a875843fd20ad6bbfc2a70143e839cc6f131d
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-20 18:33:42 +0530

    updated code

---
+++ queue/basic.java
@@ -0,0 +1,22 @@
+package queue;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class basic {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q);
+        q.remove();
+        System.out.println(q);
+        q.poll();
+        System.out.println(q);
+
+        
+    }
+    
+}
