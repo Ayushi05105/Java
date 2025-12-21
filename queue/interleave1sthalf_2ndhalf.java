65b967ca320a4e5f48c70cacb8203be4f2bfe927
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-22 02:03:35 +0530

    updated code

--- queue/interleave1sthalf_2ndhalf.java
+++ queue/interleave1sthalf_2ndhalf.java
@@ -4,6 +4,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
+// 1-> move 1st half to stack
+// 2-> empty stack to queue
+// 3-> move 1st half to stack
+// 4-> one by one add st.peek & q.peek to q
+
 public class interleave1sthalf_2ndhalf {
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
@@ -15,16 +20,16 @@ public class interleave1sthalf_2ndhalf {
         q.add(6);
         q.add(7);
         q.add(8);
-        System.out.println(q);
+        System.out.println(q);   //1 2 3 4 5 6 7 8
 
         Stack<Integer> st = new Stack<>();
         int n = q.size();
         for(int i =1;i<=n/2;i++){
-            st.push(q.remove()); //5 6 7 8
+            st.push(q.remove()); //5 6 7 8      top-> 4 3 2 1
         }
         while(st.size()>0){
             q.add(st.pop());  // 5 6 7 8 4 3 2 1
-        }
+        }                     //(-Stack-)(-queue-)
         for(int i =1;i<=n/2;i++){
             st.push(q.remove());
         }  //4 3 2 1         top 8 7 6 5
