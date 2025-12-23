4971e460ba48261159c88fe300cbfb2f9403d986
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-23 19:42:43 +0530

    updated code

---
+++ Array/Subarray.java
@@ -0,0 +1,15 @@
+public class Subarray {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        for(int i =0;i<arr.length;i++){
+            for(int j =0;j<arr.length;j++){
+                for(int k =i;k<=j;k++){
+                    System.out.print(arr[k]+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
