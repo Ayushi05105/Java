0e33edefa8ea67ac3615626ef6be78deca917107
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-01-09 18:38:54 +0530

    updated code

--- Trees/balancedBinaryTree.java
+++ Trees/balancedBinaryTree.java
@@ -1,15 +1,20 @@
 package Trees;
-
-import javax.swing.tree.TreeNode;
-
 public class balancedBinaryTree {
+     public static class Node{
+        int val;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.val = val;
+        }
+    }
 
-    public int height(TreeNode root){
+    public static int height(Node root){
         if(root ==null || (root.left ==null && root.right == null))
           return 0;
         return 1+ Math.max(height(root.left),height(root.right));  
     }
-    public boolean isBalanced(TreeNode root) {
+    public static boolean isBalanced(Node root) {
         if(root == null) return true;
         int leftHeight = height(root.left);
         if(root.left !=null)  leftHeight++;
@@ -20,5 +25,20 @@ public class balancedBinaryTree {
         if(d>1) return false;
         return (isBalanced(root.left) && isBalanced(root.right));
     }
-    
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        root.left = a;
+        root.right = b;
+        Node c = new Node(4);
+        Node d = new Node(5);
+        a.left = c;
+        a.right = d;
+        Node e = new Node(6);
+        Node f= new Node(8);
+        b.left =e;
+        b.right = f;
+        System.out.println(isBalanced(root));
+    }
 }
