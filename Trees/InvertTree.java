b28fb1721b07e6b2908634e8094143a918c86826
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-01-18 03:09:44 +0530

    updated code

---
+++ Trees/InvertTree.java
@@ -0,0 +1,23 @@
+package Trees;
+
+import javax.swing.tree.TreeNode;
+
+public class InvertTree {
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.val = val;
+        }
+    }
+    public static Node invertTree(Node root) {
+        if(root == null) return root;
+        Node l = root.left;
+        Node r = root.right;
+        root.left = invertTree(r);
+        root.right =invertTree(l);
+        return root;
+        
+    }
+}
