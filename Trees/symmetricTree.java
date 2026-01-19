24a52cf957ead446c27e2a125d678b224cd33922
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-01-19 18:39:50 +0530

    updated code

---
+++ Trees/symmetricTree.java
@@ -0,0 +1,37 @@
+package Trees;
+
+
+public class symmetricTree {
+
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.val = val;
+        }
+    }
+    public boolean SameTree(Node p ,Node q){
+     if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val != q.val) return false;
+        return SameTree(p.left,q.left) && SameTree(p.right,q.right);
+}
+
+ public Node invertTree(Node root) {
+        if(root == null) return root;
+        Node l = root.left;
+        Node r = root.right;
+        root.left = invertTree(r);
+        root.right =invertTree(l);
+        return root; 
+    }
+    
+
+    public boolean isSymmetric(Node root) {
+        if(root == null) return true;
+        root.left =invertTree(root.left);
+        return SameTree(root.left,root.right);
+    }
+    
+}
