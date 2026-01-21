83929b4861bee5e796f6b8133a15ac4cce0b7b21
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-01-21 23:32:37 +0530

    updated code

---
+++ Trees/FlattenBinaryTree.java
@@ -0,0 +1,32 @@
+package Trees;
+
+import javax.swing.tree.TreeNode;
+
+public class FlattenBinaryTree {
+    public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+    public static void flatten(Node root){
+        if(root == null) return;
+       // if(root.left == null && root.right == null) return;
+        Node leftTree = root.left;
+        Node rightTree = root.right;
+        root.left = null;
+        flatten(leftTree);
+        flatten(rightTree);
+        root.right = leftTree;
+        Node temp = leftTree;
+        while( temp != null && temp.right != null){
+            temp = temp.right;
+        }
+        if(temp != null) temp.right = rightTree;
+        else root.right = rightTree;
+        return;
+    }
+}
