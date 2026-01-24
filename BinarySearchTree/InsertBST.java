93f710821ba5c88dc31f34e6cd6126d6b05af7f5
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-01-24 20:23:21 +0530

    updated code

---
+++ BinarySearchTree/InsertBST.java
@@ -0,0 +1,48 @@
+public class InsertBST {
+
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+        public  Node(int val){
+            this.val = val;
+        }
+    }
+    public static Node insertBST(Node root, int val){
+        if(root == null) return new Node(val);
+        if(root.val > val){
+            if(root.left == null) root.left = new Node(val);
+            else insertBST(root.left, val);
+        }
+        if(root.val < val){
+            if(root.right == null) root.right = new Node(val);
+            else insertBST(root.right, val);
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(50);
+        Node a = new Node(20);
+        Node b = new Node(17);
+        Node c = new Node(34);
+        Node d = new Node(10);
+        Node e = new Node(28);
+        Node f = new Node(14);
+        Node g = new Node(60);
+        Node h = new Node(55);
+        Node i = new Node(89);
+        Node j = new Node(70);
+        root.left = a;
+        root.right = g;
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        d.right = f;
+        c.left = e;
+        g.left = h;
+        g.right = i;
+        i.left = j;
+        System.out.println(insertBST(root, 5));
+    }
+    
+}
