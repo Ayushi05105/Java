049b377262ba700d5f7fdc2eb481d84486fa5594
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-01-25 23:01:01 +0530

    updated code

---
+++ BinarySearchTree/lowestCommonAncestor.java
@@ -0,0 +1,19 @@
+public class lowestCommonAncestor {
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.val = val;
+        }
+    }
+    public static Node LowestCommonAncestor(Node root, Node p, Node q) {
+        if(p.val == root.val || q.val == root.val) return root;
+        else if(p.val<root.val && q.val>root.val) return root;
+        else if(q.val<root.val && p.val>root.val) return root;
+        else if(p.val<root.val && q.val<root.val) return LowestCommonAncestor(root.left,p,q);
+        else return LowestCommonAncestor(root.right,p,q);
+    }
+   
+    
+}
