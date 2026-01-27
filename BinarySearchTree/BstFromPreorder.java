378b7d38e19cb127902aa5f6d05b4af360fd7179
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-01-27 18:48:52 +0530

    updated code

--- BinarySearchTree/BstFromPreorder.java
+++ BinarySearchTree/BstFromPreorder.java
@@ -9,7 +9,7 @@ public class BstFromPreorder {
             this.val = val;
         }
     }
-       public static Node insertIntoBST(Node root,int val){
+    public static Node insertIntoBST(Node root,int val){
         if(root == null) return new Node(val);
         if(root.val>val){
             if(root.left == null) root.left = new Node(val);
@@ -27,6 +27,29 @@ public class BstFromPreorder {
             root =insertIntoBST(root,preorder[i]); 
         }
         return root;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(50);
+        Node a = new Node(20);
+        Node b = new Node(17);
+        Node c = new Node(34);
+        Node d = new Node(10);
+        Node e = new Node(28);
+        Node f = new Node(14);
+        Node g = new Node(60);
+        Node h = new Node(55);
+        Node i = new Node(89);
+        Node j = new Node(70);
+        root.left = a;
+        root.right = g;
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        d.right = f;
+        c.left = e;
+        g.left = h;
+        g.right = i;
+        i.left = j;
     }
     
 }
