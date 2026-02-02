5142a04d8ae803777e9153f6b989d6a10b50864a
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-02 14:45:34 +0530

    updated code

--- BinarySearchTree/TrimBST.java
+++ BinarySearchTree/TrimBST.java
@@ -8,6 +8,7 @@ public class TrimBST {
             this.val = val;
         }
     }
+    
      public static void helper(Node root, int low,int high){
         if(root == null) return;
         while(root.left != null){
