0e02f65e94f9cef209f7431217486e9c7025a560
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-08 16:24:24 +0530

    updated code

--- Hashset/distinctElement.java
+++ Hashset/distinctElement.java
@@ -2,10 +2,17 @@ package Hashset;
 import java.util.HashSet;
 
 public class distinctElement {
-    public static void main(String[] args) {
+    public static int distinct(int[] arr){
         HashSet<Integer> set = new HashSet<>();
-        set.add(1);set.add(1);set.add(1);set.add(2);
-        System.out.println("Distinct Element: "+ set.size());
+        for(int ele : arr) set.add(ele);
+        return set.size();
+    }
+    public static void main(String[] args) {
+        // HashSet<Integer> set = new HashSet<>();
+        // set.add(1);set.add(1);set.add(1);set.add(2);
+        // System.out.println("Distinct Element: "+ set.size());
+        int[] arr = {1,1,2,3};
+        System.out.println(distinct(arr));
 
     }
     
