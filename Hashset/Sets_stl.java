095305108f84fa18b3a532c2ccafec531e73cc58
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-08 16:16:43 +0530

    updated code

---
+++ Hashset/Sets_stl.java
@@ -0,0 +1,18 @@
+package Hashset;
+
+import java.util.HashSet;
+
+public class Sets_stl {
+    public static void main(String[] args) {
+     HashSet<Integer> set = new HashSet<>();
+     set.add(7); set.add(89); set.add(76);
+     set.add(80);set.add(24);
+     System.out.println(set.size());
+     System.out.println(set.contains(89)+" "+ set.contains(21));
+     set.remove(7);
+     System.out.println(set.size());
+    }
+   
+
+    
+}
